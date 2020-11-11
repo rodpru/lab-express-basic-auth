150202e4f8c05e0b827e7ef16db64f3bef89e49d
rodpru <rgp.prt@gmail.com>
2020-11-11 17:24:03 +0000

    it3

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+function requireLogin(req, res, next) {
+    if (req.session.currentUser) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  }
+  
 
+/* GET home page */
+router.get('/', (req, res, next) => {
+    req.app.locals.loggedUser = req.session.currentUser; 
+    res.render('index', {user: req.session.currentUser});
+});
 router.get('/login', (req, res) =>{
     res.render('login');
 });
+
+router.get('/main', requireLogin, (req,res) =>{
+    res.render('main');
+})
+router.get('/private', requireLogin, (req, res) =>{
+    res.render('private');
+})
   
 
 
